b29000132eb5ee9b5e2fd1ee76077088258668b1
Neil Weintraut <neil.weintraut@gmail.com>
2013-05-08 21:26:27 -0400

    End of routing. Now templating.

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.get('/page', routes.mrpage);
-app.get('/:page/*', routes.anypage);
+app.get('/:page/*git add .', routes.anypage);
 // Limit route to those with only letters in it.
 app.get('/:page([a-zA-Z]+)', routes.anypage);
 
---
+++ profiles.js
@@ -0,0 +1,26 @@
+module.exports = {
+    ryan: {
+        name: "Ryan Dahl",
+        irc: 'ryah',
+        twitter: 'ryah',
+        github: 'ry',
+        location: 'San Francisco, USA',
+        description: "Creator of node.js"
+    },
+    isaac: {
+        name: 'Isaac Schlueter',
+        irc: 'isaacs',
+        twitter: 'izs',
+        github: 'isaacs',
+        location: 'San Francisco, USA',
+        description: 'Author of npm, core contributor'
+    },
+    tj: {
+        name: "TJ Holowaychuck",
+        irc: 'tjholowaychuk',
+        twitter: 'tjholowaychuk',
+        github: 'visionmedia',
+        location: 'Victoria, BC, Canada',
+        description: 'Author of express, jande and other populate modules'
+    }
+};
