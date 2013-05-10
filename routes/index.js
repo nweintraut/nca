a3c1ab64ed44b64e7112c58c4c444f4953fe63e2
Neil Weintraut <neil.weintraut@gmail.com>
2013-05-10 00:43:58 -0400

    Javascript embedded in Jade.

--- routes/index.js
+++ routes/index.js
@@ -2,27 +2,9 @@
 /*
  * GET home page.
  */
+var profiles = require('../profiles');
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' , profiles: profiles});
 };
 
-exports.mrpage = function(req, res) {
-    res.send('Hello I am Mr. Page');
-};
-exports.anypage = function(req, res){
-    var subpage = req.params[0];
-    var parentPage = subpage ?  ' of the ' + req.params.page + ' page' : '';
-    res.send('Welcome to the ' + (subpage || req.params.page) + ' page' + parentPage);
-    
-};
-exports.anypageAdmin = function(req, res) {
-  var admin = req.params.admin;
-  if(admin) {
-      if(['add', 'delete'].indexOf(admin)!== -1) {
-          res.send('So you want to ' + req.params.admin + ' ' + req.params.page + "?");
-          return;
-      }
-      res.send(404);
-  }
-};
