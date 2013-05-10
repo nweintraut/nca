0c31ff8a348b20fdcd9a1e3f6e2ff34c11a4e7a0
Neil Weintraut <neil.weintraut@gmail.com>
2013-05-10 01:28:20 -0400

    Stylus

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
+  app.use(require('stylus').middleware({src: __dirname + '/views', dest: __dirname + '/public'}));
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
